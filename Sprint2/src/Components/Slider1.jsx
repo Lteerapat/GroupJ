f5359c3932dbd0b0f86bcb61af9dd533f902036b
Teerapong <banmairuruy@gmail.com>
2023-05-01 19:58:02 +0700

    update dot

--- Sprint2/src/Components/Slider1.jsx
+++ Sprint2/src/Components/Slider1.jsx
@@ -1,13 +1,5 @@
 import "../Styles/Slider1.css";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay , } from "swiper";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
+import { useState, useEffect, useRef } from "react";
 
 // Import Image
 import Image1 from "../Images/Landing/slide-top-1.png";
@@ -15,26 +7,69 @@ import Image2 from "../Images/Landing/slide-top-2.png";
 import Image3 from "../Images/Landing/slide-top-3.png";
 
 const Slider = () => {
+  const timeRef = useRef(null);
+  const slideImage = [Image1, Image2, Image3];
+  const [Image, setImage] = useState(0);
+
+  const next = () => {
+    if (Image < slideImage.length - 1) {
+      setImage(Image + 1);
+    } else {
+      setImage(0);
+    }
+  };
+
+  const prev = () => {
+    if (Image < 1) {
+      setImage(slideImage.length - 1);
+    } else {
+      setImage(Image - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (timeRef.current) {
+      clearTimeout(timeRef.current);
+    }
+    timeRef.current = setTimeout(() => {
+      next();
+    }, 3000);
+
+    return () => clearTimeout(timeRef.current);
+  });
+
+  const dotImage = (indexImg) => {
+    setImage(indexImg);
+  };
+
+  console.log(Image);
+  console.log(slideImage.length);
+
   return (
     <div className="slider-top">
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-        slidesPerView={1}
-        navigation
-        autoplay={{ delay: 4000 }}
-        pagination={{ clickable: true }}
-        
-      >
-        <SwiperSlide>
-          <img src={Image1} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image2} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image3} />
-        </SwiperSlide>
-      </Swiper>
+      <div className="slider-top-img">
+        <img className="slider-img" src={slideImage[Image]} />
+      </div>
+      <div className="slider-top-dot">
+        <span
+          id={Image === 0 ? "span-active" : ""}
+          onClick={() => dotImage(0)}
+        ></span>
+        <span
+          id={Image === 1 ? "span-active" : ""}
+          onClick={() => dotImage(1)}
+        ></span>
+        <span
+          id={Image === 2 ? "span-active" : ""}
+          onClick={() => dotImage(2)}
+        ></span>
+      </div>
+      <button className="pervious" onClick={() => prev()}>
+        &#8249;
+      </button>
+      <button className="next" onClick={() => next()}>
+        &#8250;
+      </button>
     </div>
   );
 };
