609c248146cfbdd6e61ef9d558fd5acd4b07cae6
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-05-09 09:08:58 +0700

    normallize landing page

--- Sprint2/src/Pages/Landing.jsx
+++ Sprint2/src/Pages/Landing.jsx
@@ -16,7 +16,6 @@ const Landing = () => {
             <a style={{'padding-right': '10px'}} href='/contactus'>Contact Us</a>
             <a style={{'padding-right': '10px'}} href='/add'>Add</a>
             <a style={{'padding-right': '10px'}} href='/edit'>Edit</a>
-            <ImageSlider_3 />
         </>
     );
 };
