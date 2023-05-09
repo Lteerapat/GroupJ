496611e214ef99dcf121a86188c500d24050c6c4
Sirimas2828 <biwsiri.k@gmail.com>
2023-05-09 19:39:42 +0700

    re push navNfooter

--- Sprint2/src/Components/Navbar.jsx
+++ Sprint2/src/Components/Navbar.jsx
@@ -1,10 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './Navbar.css';
+import logo from '../images/Logo/gaintrack-logo-light.png';
+
 
 const Navbar = () => {
 
     return (
-        <>
-            Nav
-        </>
+        
+            <div className="navBar">
+                <div className="left-navbar">
+                    <div className="logo">
+                        <a href='/'><img src= {logo} alt="logo-pic" width={200} /></a>
+                    </div>
+                </div>
+                    <div className="middle-navbar">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">Feature</a>
+                                </li>
+                                <li>
+                                    <a href="#">My Journey</a>
+                                </li>
+                                <li>
+                                    <a href="#">Our Customers</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                <div className="right-navbar">
+                            <ul>
+                                <li>
+                                <a href="/login"><button className="login">Login</button></a>
+                                </li>
+                                <li>
+                                <a href="/signup"><button className="signup">SignUp</button></a>
+                                </li>
+                            </ul>
+                </div>
+            </div>
     );
 };
     
