8fa8af935059fd58d977a426a3749335784e591a
sspsi77 <sspsiri4593@gmail.com>
2023-05-10 10:03:30 +0700

    add edit main3

--- Sprint2/src/main.jsx
+++ Sprint2/src/main.jsx
@@ -57,6 +57,11 @@ const router = createBrowserRouter([
         element: <ContactUs />,
         errorElement: <Error />
     },
+    {
+        path: '/slider2',
+        element: <Slider2 />,
+        errorElement: <Error />
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
