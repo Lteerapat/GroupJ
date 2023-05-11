acf66e3fbf33aebab9547a56a4e6c28c76440085
Teerapong <banmairuruy@gmail.com>
2023-05-11 16:43:20 +0700

    update Distance

--- Sprint2/src/Pages/Edit.jsx
+++ Sprint2/src/Pages/Edit.jsx
@@ -25,8 +25,13 @@ const Edit = () => {
   //Duration
   const [duration,setDuration] = useState()
 
+  //Distance
+  const [distance,setDistance] = useState()
+
   //Note
   const [note,setNote] = useState()
+
+
 
   return (
     <div className="container-add">
@@ -35,7 +40,8 @@ const Edit = () => {
         <NameActivity nameActivity={nameActivity} setNameActivity={setNameActivity}/>
         <TypeActivity handleActivity={handleActivity} activity={activity} />
         <DateAcitvity date={date} setDate={setDate} />
-        <DurationNote duration={duration} setDuration={setDuration} note={note} setNote={setNote} />
+        <DurationNote duration={duration} setDuration={setDuration} distance={distance} setDistance={setDistance} />
+        <Note note={note} setNote={setNote} />
         <Buttom submitButton={submitButton} />
       </div>
     </div>
@@ -46,7 +52,7 @@ const NameActivity = (props) => {
   const { nameActivity,setNameActivity } = props;
   return (
     <div className="NameActivity">
-      <label>Activity Name : {nameActivity} </label>
+      <label>Activity Name  {nameActivity} </label>
       <br />
       <input onChange={(event)=> setNameActivity(event.target.value)} type="text" ></input>
     </div>
@@ -57,7 +63,7 @@ const TypeActivity = (props) => {
   const { handleActivity, activity } = props;
   return (
     <div className="TypeActivity">
-      <label>Activity Type : {activity}</label>
+      <label>Activity Type  {activity}</label>
       <ul>
         <li
           id={activity === "walking" ? "SeleteActive" : ""}
@@ -104,7 +110,7 @@ const DateAcitvity = (props) => {
   const {date ,setDate} = props
   return (
     <div className="DateAcitvity">
-      <label>Date : {date}</label>
+      <label>Date  {date}</label>
       <br />
       <input type="date" onChange={(event)=> setDate(event.target.value)} placeholder="Date / Month / Year"></input>
     </div>
@@ -112,23 +118,34 @@ const DateAcitvity = (props) => {
 };
 
 const DurationNote = (props) => {
-  const { duration,setDuration,note,setNote} = props
-  const navigate = useNavigate();
-  const submitButton = () => {
-    navigate("/dashboard");
-  };
+  const { duration,setDuration,distance,setDistance} = props
+
 
   return (
-    <div className="DurationNote">
+    <div className="DurationDistance">
       <div>
-        <label>Duration :{duration}</label>
+        <label>Duration {duration}</label>
         <br />
         <input type="number" onChange={(event)=> setDuration(event.target.value)}/><span>min</span>
       </div>
       <div>
-        <label>Note: {note}</label>
+        <label>Distance {distance}</label>
         <br />
-        <input type="text" onChange={(event)=> setNote(event.target.value)}/>
+        <input type="number" onChange={(event)=> setDistance(event.target.value)}/><span>meter</span>
+      </div>
+    </div>
+  );
+};
+
+const Note = (props) => {
+  const { note,setNote} = props
+
+  return (
+    <div className="Note">
+      <div>
+        <label for="note-area">Note</label>
+        <br />
+        <textarea name="note-area" rows="4" cols="41" onChange={(event)=> setNote(event.target.value)}></textarea>
       </div>
     </div>
   );
@@ -137,7 +154,7 @@ const DurationNote = (props) => {
 const Buttom = (props) => {
   const { submitButton } = props;
   return (
-    <div className="Buttom">
+    <div className="Buttom-Add-Edit">
       <button onClick={submitButton}>Save</button>
       <button onClick={submitButton}>Cancel</button>
     </div>
