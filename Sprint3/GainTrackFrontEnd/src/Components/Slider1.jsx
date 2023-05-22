ecff4cb04fd7026b010311d3056fcff78c94fb2e
Teerapong <banmairuruy@gmail.com>
2023-05-22 16:03:29 +0700

    update slider1

--- Sprint3/GainTrackFrontEnd/src/Components/Slider1.jsx
+++ Sprint3/GainTrackFrontEnd/src/Components/Slider1.jsx
@@ -1,82 +1,107 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import "../Styles/Slider1.css";
-import { useState, useEffect, useRef } from "react";
+
+import Image1 from "../Images/Landing//slide-top-1.png";
+import Image2 from "../Images/Landing//slide-top-2.png";
+import Image3 from "../Images/Landing//slide-top-3.png";
+
 import { Link } from "react-router-dom";
 
-// Import Image
-import Image1 from "../Images/Landing/slide-top-1.png";
-import Image2 from "../Images/Landing/slide-top-2.png";
-import Image3 from "../Images/Landing/slide-top-3.png";
+const data = [
+  {
+    id: 1,
+    image: Image1,
+    name: "maria ferguson",
+  },
+  {
+    id: 2,
+    image: Image2,
+    buttonJU: <button className="join-us-slider-top">Join US</button>,
+  },
+  {
+    id: 3,
+    image: Image3,
+    buttonJU: <button className="join-us-slider-top">Join US</button>,
+  },
+];
 
-const Slider = () => {
-  const timeRef = useRef(null);
-  const slideImage = [Image1, Image2, Image3];
-  const [Image, setImage] = useState(0);
-
-
-  const next = () => {
-    if (Image < slideImage.length - 1) {
-      setImage(Image + 1);
-    } else {
-      setImage(0);
-    }
-  };
-
-  const prev = () => {
-    if (Image < 1) {
-      setImage(slideImage.length - 1);
-    } else {
-      setImage(Image - 1);
-    }
-  };
+function Slider() {
+  const [images, setImages] = useState(data);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    if (timeRef.current) {
-      clearTimeout(timeRef.current);
+    const lastIndex = images.length - 1;
+    //เมื่อ index น้อยกว่า 0  ให้  setIndex(2); ให้ไปต่อรูปสุดท้าย
+    if (index < 0) {
+      setIndex(lastIndex);
     }
-    timeRef.current = setTimeout(() => {
-      next();
-    }, 4000);
+    //เมื่อ index มากกว่า lastIndex(=2) ให้  setIndex(0); ให้ไปเริ่มรูปแรก
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, images]);
 
-    return () => clearTimeout(timeRef.current);
-  });
+  // autoslide
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 4000);
+    return () => clearInterval(slider);
+  }, [index]);
 
   const dotImage = (indexImg) => {
-    setImage(indexImg);
+    setIndex(indexImg);
   };
 
-  
-
   return (
-    <div className="slider-top">
-      <div className="slider-top-img">
-        <img className="slide-top-img" src={slideImage[Image]} />
+    <section className="slider-top">
+      <div className="slider-section-center">
+        {images.map((picture, pictureIndex) => {
+          const { id, image, buttonJU } = picture;
+          let position = "nextSlide";
+          if (pictureIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            pictureIndex === index - 1 ||
+            (index === 0 && pictureIndex === images.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <div className="slide-top-image">
+              <article key={id} className={position}>
+              <img src={image} className="picture-img" />
+              <Link to="/signup">{buttonJU}</Link>
+            </article>
+            </div>
+            
+          );
+        })}
+
+        <button className="slide-top-prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="slide-top-next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
       </div>
       <div className="slider-top-dot">
-        <span
-          id={Image === 0 ? "span-active" : ""}
-          onClick={() => dotImage(0)}
-        ></span>
-        <span
-          id={Image === 1 ? "span-active" : ""}
-          onClick={() => dotImage(1)}
-        ></span>
-        <span
-          id={Image === 2 ? "span-active" : ""}
-          onClick={() => dotImage(2)}
-        ></span>
+        {images.map((dots, dotIndex) => {
+          return (
+            <span
+              id={index === dotIndex ? "span-active" : ""}
+              className="slider-top-dots"
+              key={dotIndex}
+              onClick={() => dotImage(dotIndex)}
+            ></span>
+          );
+        })}
       </div>
-      <button className="pervious-slider-top" onClick={() => prev()}>
-        &#8249;
-      </button>
-      <button className="next-slider-top" onClick={() => next()}>
-        &#8250;
-      </button>
-      <Link to="/signup">
-        <button id={Image === 0 ? "join-us-slider-top-none" : " "} className="join-us-slider-top">
-          Join US
-        </button>
-      </Link>
-    </div>
+    </section>
   );
-};
+}
+
 export default Slider;
