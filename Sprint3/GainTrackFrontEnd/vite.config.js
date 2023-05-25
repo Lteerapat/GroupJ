f0338d0572879a97410634bb30c7cc2d3944e659
Wipoo Suksumek <124798841+wipoosuk@users.noreply.github.com>
2023-05-25 23:38:13 +0700

    add docker files for front and back

--- Sprint3/GainTrackFrontEnd/vite.config.js
+++ Sprint3/GainTrackFrontEnd/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: process.env.VITE_APP_HOST
+  server: {
+    watch: {
+      usePolling: true,
     },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  },
   plugins: [react()],
-})
+});
